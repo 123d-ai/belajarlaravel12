9f6f72e9e866f736b6dfb03d035c7a04f0f80dbf
Ayu Wandira <dea271204@gmail.com>
2026-01-29 22:06:00 +0700

    menambah route blog

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return "<h1> Hello World </h1>";
 });
+Route::get('/blog', function () {
+    return "<h1> ini halaman blog </h1>";
+});
